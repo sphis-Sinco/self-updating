5f989291f73c6979ccc2d4ffe2496fca57bbcb4d
IdealistCat <mattallan.h@gmail.com>
2025-01-15 19:29:54 -0600

    assets_to_download field

--- source/selfupdating/VersionFile.hx
+++ source/selfupdating/VersionFile.hx
@@ -5,6 +5,7 @@ typedef VersionFile =
 	var version:String;
 	var releasenote:String;
 	var updatetype:UpdateTypes;
+	var ?assets_to_download:Array<String>;
 }
 
 enum abstract UpdateTypes(String) from String to String
